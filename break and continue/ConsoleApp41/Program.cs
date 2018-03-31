3e142d7f924de7ff5ec5825919660e71e578741a
sahincabbar <36867588+sahincabbar@users.noreply.github.com>
2018-03-31 14:47:34 +0400

    tasklar oop

---
+++ break and continue/ConsoleApp41/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // continue aid numune
+
+            for (int i = 0; i <= 15; i++)
+            {
+
+                if (i == 10)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+
+
+
+            // break aid numune
+
+            for (int i = 0; i <= 15; i++)
+            {
+
+                if (i == 10)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+            }
+
+
+
+        }
+    }
+}
---
+++ task 6/task 6/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Man kishi = new Man();
+
+            kishi.Ichmek();
+
+            kishi.Yatmaq();
+
+            kishi.Yemek();
+
+
+            Woman qadin = new Woman();
+
+            qadin.Yemek();
+
+
+            qadin.Yatmaq();
+
+
+            qadin.Ichmek();
+        }
+    }
+
+
+
+    class Human {
+
+        public void Yemek()
+        {
+            Console.WriteLine("yemek");
+        }
+
+        public void Ichmek()
+        {
+
+            Console.WriteLine("ichmek");
+        }
+        
+        public void Yatmaq()
+        {
+            Console.WriteLine("yatmaq");
+        }
+
+
+
+    }
+
+    class Man : Human
+    {
+
+        public void MashinSurmek()
+        {
+            Console.WriteLine("mashinsurmek");
+
+
+        }
+
+        public void EsgerlikEtmek()
+        {
+            Console.WriteLine("esgerliketmek");
+
+        }
+
+
+
+    }
+
+    class Woman: Human
+    {
+        public void MakeupEtmek()
+        {
+            Console.WriteLine("makeupetmek");
+
+        }
+
+
+        public void QeybetEtmek()
+        {
+            Console.WriteLine("qeybetetmek");
+
+
+        }
+
+
+
+
+
+    }
+}
+
+
+
